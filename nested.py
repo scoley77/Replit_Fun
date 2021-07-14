08680a9d8efd51c0b30dfd5db618e2182b4e85c3
Sara Coley <79733957+scoley77@users.noreply.github.com>
2021-07-14 18:49:43 +0000

    added functions for dictionaries of nested lists

---
+++ nested.py
@@ -0,0 +1,25 @@
+cousins = {
+  'gardners' : ['Xander', 'Callan'],
+  'coleys' : ['Jay', 'Wiley'],
+  'andersons' : ['Tannon', 'Barrett', 'Graham', 'Ava'],
+  'our family' : ['Juniper', 'Winry', 'Kylan']
+}
+
+print(cousins['gardners'])
+print(cousins['andersons'][2])
+
+def first_born(dict):
+  list = []
+  for key in dict.values():
+    list.append(key[0])
+  return list
+  
+print(f'Oldest children: ' + str(first_born(cousins)))
+
+def youngest(dict):
+  list = []
+  for key in dict.values():
+    list.append(key[-1])
+  return list
+
+print(f'Youngest children: ' + str(youngest(cousins)))
